3813b0b871796c35201d82066f5bc9d2f49f7811
cloudhuh <claudiu.huhulia@gmail.com>
2023-09-11 17:03:36 +0200

    axios api call

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <script>
-export default
+import axios from 'axios'
+const endpoint = 'http://127.0.0.1:8000/api/projects/'
+import AppHeader from './components/AppHeader.vue'
+export default {
+  components: { AppHeader },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => { this.projects = res.data })
+    }
+  },
+  created() {
+    this.fetchProjects()
+  }
+
+
+};
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <main class="container mt-3">
+    <h1>ciao</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+    </ul>
+  </main>
+</template>
+
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+
+
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style></style>
